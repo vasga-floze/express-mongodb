2f58107d86ba708bf2fb649261224069ef979091
@vasga-floze <gabriela.floze@gmail.com>
2021-05-29 01:16:21 -0600

    using ejs engine for the views

--- introExpress/app.js
+++ introExpress/app.js
@@ -1,4 +1,29 @@
-const frutas = ['platano', 'aguacate', 'mango'];
-frutas.forEach(item => {
-    console.count(item)
+const express = require('express')
+const app = express()
+const port = 8080;
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views',__dirname + '/views');
+
+
+//If there exists files in this folder, this will be read first
+app.use(express.static(__dirname + '/public')); //Static file service in Express
+
+
+app.get('/', (req, res) => {
+    res.render("index", {titulo: "Este es el inicio"})
+})
+
+app.get('/about', (req, res)=> {
+    res.render("about", {tituloAbout: "Este es un mensaje dinamico de about"}) //this variable tituloAbout it will be send to about.ejs
+});
+
+//if doesn't find a page it gonna show th errorPage
+app.use('/', (req, res, next) => {
+  res.status(404).render("errorPage")
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
