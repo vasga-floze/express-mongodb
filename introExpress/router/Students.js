168c9cefb7da1dd70b32b5ab3ee329ca016355f1
@vasga-floze <gabriela.floze@gmail.com>
2021-05-29 04:08:37 -0600

    add the method create to the CRUD

--- introExpress/router/Students.js
+++ introExpress/router/Students.js
@@ -16,6 +16,28 @@ router.get('/', async (req, res) =>{
     } catch (error) {
         console.log(error)
     }
+});
+
+router.get('/create', (req, res)=>{
+    res.render('create')
+});
+
+router.post('/', async(req, res)=>{
+    const body = req.body
+    try {
+        /*
+        //this works but there is a shorter way to do the same
+        const studentDB = new Student(body)
+        await studentDB.save()*/
+
+        await Student.create(body); //this is the shorter way to do it
+
+        //console.log('new student: ' + studentDB)
+        res.redirect('/students');//this redirect to the user to a another page
+
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
